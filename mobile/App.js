abc6a27b188e5fa148efeaa5a4ba75fa200e56cc
DiogoSena <diogodmsena@gmail.com>
2020-04-12 21:01:00 -0300

    Projeto Be The Hero

---
+++ mobile/App.js
@@ -0,0 +1,13 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+
+//Teste push repositório Git
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
